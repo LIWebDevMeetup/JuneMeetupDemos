3a33e44b5d023bbe276ddfec8929d1535b0e304c
Glenn Ferrie <glenn.ferrie@gmail.com>
2015-06-23 14:02:59 -0400

    added analyzers

---
+++ AnalyzerDemo/AnalyzerDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyzerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class GlennType
+    {
+        public GlennType()
+        {
+
+        }
+    }
+}
